90542d201313974f34640c70631d887b4f801422
Zeba <saidzodazebida@gmail.com>
2025-11-29 16:32:14 +0300

    forth

--- app/src/main/java/com/example/affirmations/model/Affirmation.kt
+++ app/src/main/java/com/example/affirmations/model/Affirmation.kt
@@ -1,6 +1,9 @@
 package com.example.affirmations.model
 
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
 data class Affirmation(
-    val stringResourceId: Int,
-    val imageResourceId: Int
+    @StringRes val stringResourceId: Int,
+    @DrawableRes val imageResourceId: Int
 )
